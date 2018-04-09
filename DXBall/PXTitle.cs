8cd51c9f760e9d331dd3414fb70f636879114be2
isalk <isalk@hotmail.com>
2018-04-09 21:24:50 +0800

    Reformatted the code

--- DXBall/PXTitle.cs
+++ DXBall/PXTitle.cs
@@ -13,23 +13,64 @@ namespace DXBall
         private LinearGradientBrush titleBrush;
         private int alpha, x, y, width, height;
         private Color color1, color2;
-        public string Text { get; set; }
-        public int Alpha { get { return alpha; } set { alpha = value; } }
-        public int X { get { return x; } set { x = value; } }
-        public int Y { get { return y; } set { y = value; } }
-        public int Width { get { return width; } set { width = value; } }
-        public int Height { get { return height; } set { height = value; } }
-        public LinearGradientBrush TitleBrush { get { return titleBrush; } }
-        public PointF Position { get; set; }
-        public bool Focused { get; set; }
-        public Font TitleFont { get; set; }
-
+        public string Text 
+        { 
+            get; 
+            set; 
+        }
+        public int Alpha 
+        { 
+            get { return alpha; } 
+            set { alpha = value; } 
+        }
+        public int X 
+        {
+            get { return x; } 
+            set { x = value; } 
+        }
+        public int Y 
+        { 
+            get { return y; } 
+            set { y = value; } 
+        }
+        public int Width 
+        {
+            get { return width; } 
+            set { width = value; } 
+        }
+        public int Height 
+        { 
+            get { return height; } 
+            set { height = value; } 
+        }
+        public LinearGradientBrush TitleBrush 
+        { 
+            get { return titleBrush; } 
+        }
+        public PointF Position 
+        { 
+            get; 
+            set; 
+        }
+        public bool Focused 
+        { 
+            get;
+            set;
+        }
+        public Font TitleFont 
+        { 
+            get; 
+            set; 
+        }
         public event EventHandler AlphaIsMax;
         public event EventHandler AlphaIsZero;
 
         public PXTitle(Color _color1, Color _color2, float posX1, float posY1, float posX2, float posY2)
         {
-            alpha = 0; color1 = _color1; color2 = _color2; Focused = false;
+            alpha = 0; 
+            color1 = _color1;
+            color2 = _color2;
+            Focused = false;
             titleBrush = new LinearGradientBrush(new PointF(posX1, posY1), new PointF(posX2, posY2), 
                 Color.FromArgb(alpha, color1), Color.FromArgb(alpha, color2));
         }
@@ -78,6 +119,9 @@ namespace DXBall
             return x > X && x < X + Width && y > Y && y < Y + Height;
         }
 
-        ~PXTitle() { GC.Collect(); }
+        ~PXTitle() 
+        { 
+            GC.Collect();
+        }
     }
 }
