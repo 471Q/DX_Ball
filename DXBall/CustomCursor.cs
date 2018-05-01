34e4286918c7541821e7c2e09e899650fe6d4c4c
ATIQ <A_471k@yahoo.com>
2018-05-01 21:52:40 +0800

    Resource files, cursor image

---
+++ DXBall/CustomCursor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DXBall
+{
+	public class CustomCursor
+	{
+		public CustomCursor()
+		{
+		}
+	}
+}
--- DXBall/Properties/Resources.Designer.cs
+++ DXBall/Properties/Resources.Designer.cs
@@ -144,6 +144,16 @@ namespace DXBall.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap cursor {
+            get {
+                object obj = ResourceManager.GetObject("cursor", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cyanbox {
             get {
                 object obj = ResourceManager.GetObject("cyanbox", resourceCulture);
