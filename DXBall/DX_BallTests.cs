08bbe9a6b0bf5be1b99c5d0140735f648afc71b8
AydinSopian <aydin.sopian@gmail.com>
2018-05-15 22:20:18 +0800

    committed AND THEN added the DXBallTests files

--- DXBall/DX_BallTests.cs
+++ DXBall/DX_BallTests.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+using DXBall.Properties;
+using NUnit.Framework;
+
 namespace DXBall
 {
+	[TestFixture()]
 	public class DX_BallTests
 	{
-		public DX_BallTests()
+		[Test()]
+		public void CollectableCollectionCheckTest()
 		{
+			Collectables c = new MultiBall(0f, 0f);
+			DXLine l = new DXLine(0f, 50f);
+
+			c.AddLine(l);
+
+			Assert.IsFalse(c.Collected);
+
+			c.PosY = l.LineRectangle.Top;
+			c.PosX = l.LineRectangle.Right;
+
+			c.CollectionCheck(l.LineRectangle);
+
+			Assert.IsTrue(c.Collected);
 		}
 	}
 }
